17dd7dd16f845bcea6cc8c98460e87bd40597ebc
Anele Ngwekazi <anele.ngwekazi@gmail.com>
2018-12-12 11:47:52 +0200

    check if date is valid ID on the ID number

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 "use strict";
 exports.__esModule = true;
-var id = "9408144756081";
-console.log(validityTest(id));
+var moment = require("moment");
+var id = "9408144756086";
 function validityTest(id) {
     var sumOddIdPosition = 0;
     var evenIdPosition = 0;
     if (!id) {
+        console.log("ID is empty or null");
         return false;
     }
-    if (id.split('').some(function (number) { return isNaN(parseInt(number)); }) || id.length != 13) {
+    if (id.split('').some(function (number) { return isNaN(parseInt(number)); })) {
+        console.log("ID should only have numbers");
+        return false;
+    }
+    if (id.length != 13) {
+        console.log("ID length should be 13");
+        return false;
+    }
+    if (moment(id.substring(0, 6), "YYMMDD").isValid()) {
+        console.log("First 6 digits is not a valid date");
         return false;
     }
     for (var i = 0; i < 12; i += 2) {
@@ -22,6 +32,7 @@ function validityTest(id) {
     }
     var parity = 10 - (sumEvenIdPosition + sumOddIdPosition) % 10;
     if (parity != Number(id.charAt(12))) {
+        console.log("Parity number is in correct");
         return false;
     }
     return true;
--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 export {}
+import * as moment from "moment";
 
- let id = "9408144756081";
- console.log(validityTest(id));
-function validityTest(id) {
+ let id = "9408144756086";
+
+function validityTest(id: string): boolean {
     let sumOddIdPosition = 0;
     let evenIdPosition = 0;
 
     if (!id) {
+        console.log("ID is empty or null");
         return false;
     }
 
-    if (id.split('').some(number => isNaN(parseInt(number))) || id.length != 13) {
+    if (id.split('').some(number => isNaN(parseInt(number)))) { 
+        console.log("ID should only have numbers");
+        return false;
+    }
+
+    if (id.length != 13 ) {
+        console.log("ID length should be 13");
+        return false;
+    }
+
+    if (moment(id.substring(0, 6), "YYMMDD").isValid()) {
+        console.log("First 6 digits is not a valid date");
         return false;
     }
 
@@ -18,7 +31,6 @@ function validityTest(id) {
         sumOddIdPosition += Number(id.charAt(i));
         evenIdPosition = evenIdPosition * 10 + Number(id.charAt(i + 1));
     }
-
     let stringEvenIdPosition = (evenIdPosition * 2).toString();
     let sumEvenIdPosition = 0
     for (let i = 0; i < stringEvenIdPosition.length; i++) {
@@ -27,7 +39,9 @@ function validityTest(id) {
 
     let parity = 10 - (sumEvenIdPosition + sumOddIdPosition) % 10;
     if (parity != Number(id.charAt(12))) {
+        console.log("Parity number is in correct");
         return false;
-    }   
+    }
+        
     return true;
 }
